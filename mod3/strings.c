717e1f98cac7b67bc513ee9f20a1f124b7d5b4db
Austin Jack <austinjack@mail.weber.edu>
2018-01-23 10:17:36 -0700

    "Strings Practice"

---
+++ mod3/strings.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  strings.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  01/23/2018 10:14:57 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Austin Jack (), austinjack@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+char name[11];
+printf("Enter you name:\n");
+scanf("%s", name);
+printf("Your name is[%s]\n", name);
+
+    return 0;
+}
+// Function Definitions
+
+
